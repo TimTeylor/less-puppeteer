40772d6a347e0bae16f2d6cd22294e47f8dbd4df
Tim Teillor <timteillor@gmail.com>
2021-09-27 09:36:00 +0200

    Get all images from the first page

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const https = require('https');
+const { writeFile, createWriteStream } = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ headless: true })
   const page = await browser.newPage()
   await page.goto('https://yandex.ru/images/search?text=reno')
 
@@ -15,6 +17,29 @@ const puppeteer = require('puppeteer');
 
   await page.waitForSelector('.MMImage-Origin')
   await page.screenshot({ path: 'screenshot.png' })
+
+  let images = await page.evaluate(() => {
+    let imgElements = document.querySelectorAll('.serp-item__thumb')
+    let URLs = Object.values(imgElements).map(
+      el => ({
+        src: el.src,
+        alt: el.alt
+      })
+    )
+    return URLs
+  })
+
+  writeFile('imagesURL.json', JSON.stringify(images, null, ' '), err => {
+    if (err) return err
+    console.log('images -> imagesURL.json')
+  })
+
+  images.forEach((image, index) => {
+    const file = createWriteStream(`images/${index}.webp`)
+    https.get(image.src, response => {
+      response.pipe(file)
+    })
+  })
 
   await browser.close()
 })()
